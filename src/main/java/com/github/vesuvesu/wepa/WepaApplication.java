3494806dbf12b41127268a1f96dbaa4c02c137c1
Veikko Suhonen <veikko.suhonen@helsinki.fi>
2020-11-23 20:06:40 +0200

    fixed java version

--- src/main/java/com/github/vesuvesu/wepa/WepaApplication.java
+++ src/main/java/com/github/vesuvesu/wepa/WepaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WepaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WepaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WepaApplication.class, args);
+    }
 
 }
