f431dbdfca02724b73409a3575883afd40131d1d
Veikko Suhonen <veikko.suhonen@helsinki.fi>
2020-11-23 19:57:38 +0200

    initial commit

---
+++ src/main/java/com/github/vesuvesu/wepa/WepaApplication.java
@@ -0,0 +1,13 @@
+package com.github.vesuvesu.wepa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WepaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WepaApplication.class, args);
+	}
+
+}
