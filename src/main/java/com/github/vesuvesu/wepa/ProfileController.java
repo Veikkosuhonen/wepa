281c1ff43691764b9a15eb6b1291aa91ceb148ca
Veikko Suhonen <veikko.suhonen@helsinki.fi>
2020-12-08 22:03:43 +0200

    Friendrequests are now working. Added UserController to handle requests to path /users, ProfileController to handle 'myprofile' view. Renamed 'my account' to 'my profile', 'userview' to 'myprofile'.

---
+++ src/main/java/com/github/vesuvesu/wepa/ProfileController.java
@@ -0,0 +1,26 @@
+package com.github.vesuvesu.wepa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProfileController {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Secured("USER")
+    @GetMapping("/myprofile")
+    public String userView(Model model) {
+
+        User user = accountRepository.findByUsername(SecurityContextHolder.getContext()
+                .getAuthentication().getName()).getUser();
+
+        model.addAttribute("user", user);
+        return "myprofile";
+    }
+}
--- src/main/java/com/github/vesuvesu/wepa/UserController.java
+++ src/main/java/com/github/vesuvesu/wepa/UserController.java
@@ -4,23 +4,77 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class UserController {
 
     @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
     private AccountRepository accountRepository;
 
+    @Transactional
     @Secured("USER")
-    @GetMapping("/myaccount")
-    public String userView(Model model) {
+    @PostMapping("/users/{name}/friend")
+    public String friendRequest(@PathVariable String name) {
+        Account account = accountRepository.findByUsername(
+                SecurityContextHolder.getContext().getAuthentication().getName());
 
-        User user = accountRepository.findByUsername(SecurityContextHolder.getContext()
-                .getAuthentication().getName()).getUser();
+        User sender = account.getUser();
+        User receiver = userRepository.findByName(name);
 
-        model.addAttribute("user", user);
-        return "userview";
+        if (receiver != null && sender != null) {
+            if (!receiver.getFriendRequests().contains(sender))
+                receiver.getFriendRequests().add(sender);
+        }
+        return "redirect:/search";
+    }
+
+    @Transactional
+    @Secured("USER")
+    @PostMapping("/users/{name}/{action}")
+    public String resolveFriendRequest(@PathVariable String name, @PathVariable String action) {
+        Account account = accountRepository.findByUsername(
+                SecurityContextHolder.getContext().getAuthentication().getName());
+
+        User actor = account.getUser();
+        if (actor==null) {
+            System.out.println("Actor of this friend request is null");
+            return "redirect:/myprofile";
+        }
+
+        User sender = userRepository.findByName(name);
+        if (sender==null) {
+            System.out.println("Sender '"+name+"' of this friend request is null");
+            return "redirect:/myprofile";
+        }
+
+        if (actor.getFriendRequests().contains(sender)) {
+
+            if (action.equals("accept")) {
+                sender.getFriends().add(actor);
+                actor.getFriends().add(sender);
+                //Todo send accept message to sender
+
+                actor.getFriendRequests().remove(sender);
+                System.out.println(actor.getName() + " accepted friend request from " + sender.getName());
+
+            } else if (action.equals("decline")) {
+                actor.getFriendRequests().remove(sender);
+                //Todo send decline message to sender
+
+                System.out.println(actor.getName() + " declined friend request from " + sender.getName());
+
+            } else {System.out.println("Action '"+action+"' was invalid");}
+        } else {System.out.println("This friend request did not exist");}
+
+
+        return "redirect:/myprofile";
     }
 }
