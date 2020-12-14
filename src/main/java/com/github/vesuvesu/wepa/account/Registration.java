80ec0473f64fd4880e316ce4c6df64fd0c419a68
Veikko Suhonen <veikko.suhonen@helsinki.fi>
2020-12-14 13:02:16 +0200

    Registration validation and fixed public profile nav buttons

--- src/main/java/com/github/vesuvesu/wepa/account/Registration.java
+++ src/main/java/com/github/vesuvesu/wepa/account/Registration.java
@@ -5,20 +5,26 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Data
 @AllArgsConstructor @NoArgsConstructor
 public class Registration {
+
+
     @NotEmpty
+    @Pattern(regexp = "[\\w]+", message = "Your username can only contain letters, numbers and underscores")
     @Size(min = 3, max = 16, message = "Your username should be between 3 and 16 characters")
     private String username;
 
     @NotEmpty
+    @Pattern(regexp = "[\\w]+", message = "Your name can only contain letters, numbers and underscores")
     @Size(min = 3, max = 16, message = "Your name should be between 3 and 16 characters")
     private String name;
 
     @NotEmpty
+    @Pattern(regexp = "[\\S]+", message = "Your password cannot contain whitespace")
     @Size(min = 5, max = 20, message = "Your password should be between 5 and 20 characters")
     private String password;
 }
