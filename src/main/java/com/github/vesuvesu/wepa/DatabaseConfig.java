40312e23fdf2e3655796256a1bd2ab9ff8102fea
Veikko Suhonen <veikko.suhonen@helsinki.fi>
2020-12-14 18:22:22 +0200

    Database production config

---
+++ src/main/java/com/github/vesuvesu/wepa/DatabaseConfig.java
@@ -0,0 +1,21 @@
+package com.github.vesuvesu.wepa;
+
+import com.zaxxer.hikari.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import javax.sql.DataSource;
+
+@Profile("production")
+@Configuration
+public class DatabaseConfig {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        return new HikariDataSource(config);
+    }
+}
